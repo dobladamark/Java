443f7f4804310f546bead0c38d2cf2f9a8ccd949
mark <122426238+dobladamark@users.noreply.github.com>
2025-01-31 12:14:41 +0800

    Update Educ.java

--- #16 OOP - Inheritance/src/pkg16/oop/inheritance/Educ.java
+++ #16 OOP - Inheritance/src/pkg16/oop/inheritance/Educ.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pkg16.oop.inheritance;
 
-/**
- *
- * @author Nyabi
- */
+
 public class Educ extends Student{
     
     public Educ(String studName, int studGrade) {
